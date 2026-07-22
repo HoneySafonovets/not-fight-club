17984f42a0f0f4962ba7910a12b2c05619b022e5
HoneySafonovets <kfs.boi@yandex.ru>
2026-07-22 22:11:52 +0300

    feat: add basic files and basic assets

---
+++ not-fight-club/src/js/hit.js
@@ -0,0 +1,4 @@
+export default function hit() {
+  console.log(document.querySelector('.hero'))
+  document.querySelector('.hero').src = './assets/crus_hit.png'
+}
--- not-fight-club/src/main.js
+++ not-fight-club/src/main.js
@@ -1,12 +1,14 @@
-import './style.css'
+import './scss/main.scss';
+import hit from './js/hit';
 
-document.querySelector('#app').innerHTML = html`
+document.querySelector('#app').innerHTML = `
 <header class="header">
   <h1 class="header__title">Not fight club</h1>
 </header>
 <main id="main">
-
+  <img class="hero" src="./assets/crusader.png" alt="Crusader">
 </main>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+
+document.querySelector('.hero').addEventListener('click', hit);
